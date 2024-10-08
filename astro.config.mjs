3a9526488b3bd3180a997a171e056bdb246472f8
EugenAchtzehn <eugene1826@gmail.com>
2024-10-08 16:31:30 +0800

    [Fix] minor, add the site description in astro config file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://huasha.com.tw',
   integrations: [
     sitemap(),
     partytown({
