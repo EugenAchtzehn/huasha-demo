8cb0fa5116b6bb0e3dd15aa1602e8053b6944398
EugenAchtzehn <eugene1826@gmail.com>
2024-10-08 15:18:46 +0800

    [Change] major, sitemap added and fix vulnerabilities.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eugenachtzehn.github.io/',
   base: 'huasha-demo',
+  integrations: [sitemap()],
 });
